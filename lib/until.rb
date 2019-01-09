7d7afcb747c78a43ca04cff2504c536f241f6bb1
Rafael Vargas <vargas718@gmail.com>
2019-01-09 04:51:10 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,9 @@
+
 def using_until
         levitation_force = 6
-        puts ""
+        puts "Wingardium Leviosa"
+        levitation_force += 1
+        until levitation_force == 10
         
     
 end
